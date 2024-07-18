bd40e9875fa82152f95e432923ffed168d9faf8c
Madhav Kandukuri <madhav165@users.noreply.github.com>
2024-07-18 12:34:05 +0530

    add version

--- src/ragas/__init__.py
+++ src/ragas/__init__.py
@@ -5,7 +5,7 @@ from ragas.run_config import RunConfig
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown version"
+    __version__ = "0.1.8"
 
 
 __all__ = ["evaluate", "adapt", "RunConfig", "__version__"]
